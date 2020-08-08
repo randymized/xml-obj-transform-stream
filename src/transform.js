8b1bc943eb553091f2549b55fe6ded0e2f2f4109
Randy McLaughlin <8b629a9884@snkmail.com>
2020-08-08 16:21:38 -0500

    Created project, copied in code for transform, and started creating tests

---
+++ src/transform.js
@@ -0,0 +1,150 @@
+const Saxophone = require('saxophone');
+const { Transform } = require('stream');
+const Queue = require('tiny-queue');
+const { parse } = require('path');
+
+const AvailableNodes= [
+    'tagopen',
+    'tagclose',
+    'text',
+    'cdata',
+    'comment',
+    'processinginstruction',
+]
+exports.AvailableNodes= AvailableNodes
+
+exports.Saxophone= Saxophone
+exports.parseAttrs= Saxophone.parseAttrs
+exports.parseEntities= Saxophone.parseEntities
+
+
+class XMLTransform extends Transform {
+    constructor(options) {
+        const opt = Object.assign({}, options);
+        const optinclude = opt.include || AvailableNodes;
+        delete opt.include
+        const optNoEmptyText = opt.noEmptyText
+        delete opt.noEmptyText
+        const optReportSelfClosing = opt.reportSelfClosing
+        delete opt.reportSelfClosing
+        const parseAttrs= opt.hasOwnProperty('parseAttrs') ? opt.parseAttrs : true
+        delete opt.parseAttrs
+        super(Object.assign({}, opt, { readableObjectMode: true }));
+
+        this.parser = new Saxophone(); // This object is a writable stream that will emit an event for each tag or node parsed from the incoming data
+
+        this.queue = new Queue();
+        const queue = this.queue;
+
+        const wantsTagclose= optinclude.includes('tagclose')
+        function parseAttrsOrNot(attrs) {
+            if (parseAttrs && attrs) {
+                return Saxophone.parseAttrs(attrs);
+            }
+            else {
+                return {}
+            }
+        }
+        optinclude.forEach(include=>{
+            switch (include) {
+                case 'tagopen':
+                    if (wantsTagclose) {
+                        if (optReportSelfClosing) {
+                            this.parser.on(include, obj => {
+                                queue.push([include, obj.name, obj.attrs, obj.isSelfClosing])
+                                if (obj.isSelfClosing) {
+                                    queue.push(['tagclose',obj.name])
+                                }
+                            })
+                        } else {
+                            this.parser.on(include, obj => {
+                                queue.push([include, obj.name, obj.attrs])
+                                if (obj.isSelfClosing) {
+                                    queue.push(['tagclose',obj.name])
+                                }
+                            })
+                        }
+                    }
+                    else {
+                        if (optReportSelfClosing) {
+                            this.parser.on(include, obj => {
+                                queue.push([include,obj.name,obj.attrs,obj.isSelfClosing])
+                            })
+                        } else {
+                            this.parser.on(include, obj => {
+                                queue.push([include,obj.name,obj.attrs])
+                            })
+                        }
+                    }
+                    break;
+                case 'tagclose':
+                    this.parser.on(include, obj => {
+                        queue.push([include,obj.name])
+                    })
+                    break;
+                case 'text':
+                    if (optNoEmptyText) {
+                        this.parser.on(include, obj => {
+                            if (!/^\s*$/.test(obj.contents)) {
+                                queue.push([include,obj.contents])
+                            }
+                        })
+                    }
+                    else {
+                        this.parser.on(include, obj => {
+                            queue.push([include,obj.contents])
+                        })
+                    }
+                    break;
+                case 'cdata':
+                case 'comment':
+                case 'processinginstruction':
+                    this.parser.on(include, obj => queue.push([include,obj.contents]))
+                    break;
+            }
+        })
+    }
+
+    _transform(chunk, encoding, callback) {
+        new Promise((resolve,reject)=>{
+            try {
+                if (!this.parser.write(chunk,encoding)) {
+                    this.parser.once('drain', resolve)
+                }
+                else {
+                    resolve()
+                }
+            }
+            catch (e) {
+                reject(e)
+            }
+        })
+        .then(()=>{
+            callback(this._dumpQueue())
+        })
+        .catch(callback)
+    }
+
+    _dumpQueue() {
+        const queue = this.queue;
+        while (queue.length > 0) {
+            const item = queue.shift();
+            if (item[0] == 'error')
+                return item[1];
+            this.push(item);
+        }
+        return null;
+    }
+
+    _flush(callback) {
+        this.parser.end((err)=>{
+            if (err) {
+                callback(err)
+            } else {
+                callback(this._dumpQueue());
+            }
+        })
+    }
+}
+
+exports.XMLTransform = XMLTransform;
---
+++ tests/index.mocha.js
@@ -0,0 +1,214 @@
+'use strict';
+
+const {pipeline, Readable, Writable}= require('stream')
+const fs= require('fs')
+const assert = require('assert');
+const should = require('should');
+
+const tags = require('common-tags');
+const {XMLTransform}= require('../src/transform');
+const { runInThisContext } = require('vm');
+const wants= ['tagopen','text','tagclose',]
+
+const sampleXML= tags.stripIndent`
+<?xml version="1.0" encoding="UTF-8"?>
+<head>
+<!-- this is a comment -->
+<title>XML Test File</title>
+<selfclose />
+<cdata-section><![CDATA[this is a c&data s<>ction]]></cdata-section>
+<empty></empty>
+<hasattrs first="one" second="two"  third="three " />
+<textarea> this\nis\na\r\n\ttextual\ncontent  </textarea>
+<other attr="value"></other>
+</head>`
+
+class ConsoleDirWritable extends Writable {
+    constructor(options) {
+        super(Object.assign({},options, {objectMode: true}))
+    }
+    _write(chunk,encoding,callback) {
+        console.dir(chunk)
+        callback()
+    }
+}
+
+class ObjectArrayWritable extends Writable {
+    constructor(options) {
+        super(Object.assign({},options, {objectMode: true}))
+        this.accum= []
+    }
+    _write(chunk,encoding,callback) {
+        this.accum.push(chunk)
+        callback()
+    }
+}
+
+class ObjectArrayComparator extends Writable {
+    constructor(compareTo, options) {
+        super(Object.assign({},options, {objectMode: true}))
+        this.accum= []
+        this.compareTo= compareTo
+        this.index= 0
+    }
+    _write(chunk,encoding,callback) {
+        debugger
+        should.deepEqual(chunk,this.compareTo[this.index])
+        this.index+= 1
+        callback()
+    }
+}
+
+class ReadableString extends Readable {
+    constructor(str) {
+      super();
+      this.str= str
+      this.sent= false
+    }
+
+    _read() {
+      if (!this.sent) {
+        this.push(Buffer.from(this.str));
+        this.sent = true
+      }
+      else {
+        this.push(null)
+      }
+    }
+  }
+
+describe('XML Obj Transform Stream', function() {
+    it('should parse a sample that contains several elements', function(done) {
+        const sampleXMLParsed= [
+            [ 'processinginstruction', 'xml version="1.0" encoding="UTF-8"' ],
+            [ 'text', '\n' ],
+            [ 'tagopen', 'head', '' ],
+            [ 'text', '\n' ],
+            [ 'comment', ' this is a comment ' ],
+            [ 'text', '\n' ],
+            [ 'tagopen', 'title', '' ],
+            [ 'text', 'XML Test File' ],
+            [ 'tagclose', 'title' ],
+            [ 'text', '\n' ],
+            [ 'tagopen', 'selfclose', ' ' ],
+            [ 'tagclose', 'selfclose' ],
+            [ 'text', '\n' ],
+            [ 'tagopen', 'cdata-section', '' ],
+            [ 'cdata', 'this is a c&data s<>ction' ],
+            [ 'tagclose', 'cdata-section' ],
+            [ 'text', '\n' ],
+            [ 'tagopen', 'empty', '' ],
+            [ 'tagclose', 'empty' ],
+            [ 'text', '\n' ],
+            [ 'tagopen', 'hasattrs', ' first="one" second="two"  third="three " ' ],
+            [ 'tagclose', 'hasattrs' ],
+            [ 'text', '\n' ],
+            [ 'tagopen', 'textarea', '' ],
+            [ 'text', ' this\nis\na\r\n\ttextual\ncontent  ' ],
+            [ 'tagclose', 'textarea' ],
+            [ 'text', '\n' ],
+            [ 'tagopen', 'other', ' attr="value"' ],
+            [ 'tagclose', 'other' ],
+            [ 'text', '\n' ],
+            [ 'tagclose', 'head' ],
+            [ 'text', '\n' ],
+        ]
+        const objectArrayComparator = new ObjectArrayComparator(sampleXMLParsed);
+        pipeline(
+            new ReadableString(sampleXML),
+            new XMLTransform(),
+            objectArrayComparator,
+            (err) => {
+                if(err) {
+                    done(err);
+                }
+                else {
+                    done()
+                }
+            }
+        )
+
+    });
+
+    // here goes your code
+    it('should allow choosing types of elements to report', function(done) {
+        const toInclude = ['tagopen', 'tagclose'];
+        const options = { include: toInclude };
+        const sampleXMLParsed= [
+            [ 'tagopen', 'head', '' ],
+            [ 'tagopen', 'title', '' ],
+            [ 'tagclose', 'title' ],
+            [ 'tagopen', 'selfclose', ' ' ],
+            [ 'tagclose', 'selfclose' ],
+            [ 'tagopen', 'cdata-section', '' ],
+            [ 'tagclose', 'cdata-section' ],
+            [ 'tagopen', 'empty', '' ],
+            [ 'tagclose', 'empty' ],
+            [ 'tagopen', 'hasattrs', ' first="one" second="two"  third="three " ' ],
+            [ 'tagclose', 'hasattrs' ],
+            [ 'tagopen', 'textarea', '' ],
+            [ 'tagclose', 'textarea' ],
+            [ 'tagopen', 'other', ' attr="value"' ],
+            [ 'tagclose', 'other' ],
+            [ 'tagclose', 'head' ]
+        ]
+        const objectArrayComparator = new ObjectArrayComparator(sampleXMLParsed);
+        pipeline(
+            new ReadableString(sampleXML),
+            new XMLTransform(options),
+            objectArrayComparator,
+            (err) => {
+                if(err) {
+                    done(err);
+                }
+                else {
+                    done()
+                }
+            }
+        )
+
+    });
+
+    it('should allow omitting empty text', function(done) {
+        const options= {noEmptyText:true}
+        const sampleXMLParsed= [
+            [ 'processinginstruction', 'xml version="1.0" encoding="UTF-8"' ],
+            [ 'tagopen', 'head', '' ],
+            [ 'comment', ' this is a comment ' ],
+            [ 'tagopen', 'title', '' ],
+            [ 'text', 'XML Test File' ],
+            [ 'tagclose', 'title' ],
+            [ 'tagopen', 'selfclose', ' ' ],
+            [ 'tagclose', 'selfclose' ],
+            [ 'tagopen', 'cdata-section', '' ],
+            [ 'cdata', 'this is a c&data s<>ction' ],
+            [ 'tagclose', 'cdata-section' ],
+            [ 'tagopen', 'empty', '' ],
+            [ 'tagclose', 'empty' ],
+            [ 'tagopen', 'hasattrs', ' first="one" second="two"  third="three " ' ],
+            [ 'tagclose', 'hasattrs' ],
+            [ 'tagopen', 'textarea', '' ],
+            [ 'text', ' this\nis\na\r\n\ttextual\ncontent  ' ],
+            [ 'tagclose', 'textarea' ],
+            [ 'tagopen', 'other', ' attr="value"' ],
+            [ 'tagclose', 'other' ],
+            [ 'tagclose', 'head' ],
+        ]
+        const objectArrayComparator = new ObjectArrayComparator(sampleXMLParsed);
+        pipeline(
+            new ReadableString(sampleXML),
+            new XMLTransform(options),
+            objectArrayComparator,
+            (err) => {
+                if(err) {
+                    done(err);
+                }
+                else {
+                    done()
+                }
+            }
+        )
+
+    });
+
+});
